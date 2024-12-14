4fa12bf6b51246c58b5eacf880faaa378aeec500
sondosAbodahab <sondos.ahmed031@gmail.com>
2024-12-14 02:07:04 +0200

    feat: :sparkles: create register feature

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,10 +4,10 @@ const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
   try {
-    const { username, email, password } = req.body;
+    const { userName, email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({
-      username,
+      userName,
       email,
       password: hashedPassword,
     });
--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { Router } from "@angular/router";
+import { AuthResponse } from "src/app/models/Auth.Model";
+import { AuthService } from "src/app/services/auth.service";
 
+const matModules = [
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule,
+];
 @Component({
-  selector: 'app-register',
+  selector: "app-register",
   standalone: true,
-  imports: [],
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  imports: [...matModules, CommonModule, ReactiveFormsModule],
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"],
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private authService: AuthService, private router:Router) {
+    this.registerForm = this.fb.group(
+      {
+        userName: ["", [Validators.required]],
+        email: ["", [Validators.required, Validators.email]],
+        password: ["", [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ["", [Validators.required]],
+      },
+      { validators: this.passwordMatchValidator }
+    );
+  }
+
+  passwordMatchValidator(form: FormGroup) {
+    const password = form.get("password")?.value;
+    const confirmPassword = form.get("confirmPassword")?.value;
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  }
+
+  submit() {
+    if (this.registerForm.valid) {
+      this.authService.register(this.registerForm.value).subscribe({
+        next: (response) => {
+          console.log(response);
+          this.router.navigate(['/dashboard']);
+        },
+        error: (error) => console.error("Login failed", error),
+      });
+    }
+  }
 }
